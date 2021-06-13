5808bf8c5c5eae1fd6ca63c44d96d8b2a1685b61
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-06-13 19:33:19 +0100

    added swagger documentation

--- crm/settings.py
+++ crm/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'staff_crm.apps.StaffCrmConfig',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
